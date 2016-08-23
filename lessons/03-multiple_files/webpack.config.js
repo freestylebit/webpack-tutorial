0011cd5b80a53ca237fa19ad23f6652136e8d86d
Peter Cho <pcho231@gmail.com>
2016-08-23 00:27:27 -0400

    Standardized lesson 03

--- lessons/03-multiple_files/webpack.config.js
+++ lessons/03-multiple_files/webpack.config.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const webpack = require('webpack');
+const glob = require('glob');
 
 let config = {
   entry: {
-    'script1': './js/script1.js',
-    'script2': './js/script2.js',
+    'script1': './src/script1.js',
+    'script2': './src/script2.js',
   },
   output: {
     path: './dist',
-    filename: 'compressed.[name].js'
+    filename: '[name].min.js'
   }
 }
+
+// Create individual js bundles automatically!
+let pages = glob.sync('./src/bunch_of_js/**/*.js');
+pages.map((file) => {
+  let key = file.split('/');
+  key = key.slice(-1)[0];
+  key = key.replace('.js', '');
+
+  config.entry[key] = file;
+});
 
 module.exports = config;
