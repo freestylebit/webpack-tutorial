315a2696b0572d9b5c27db7590d9c7d48de7dd97
Peter Cho <pcho231@gmail.com>
2017-02-25 15:30:19 -0500

    Enhanced typescript demo

--- lessons/06-loaders/src/typescript.ts
+++ lessons/06-loaders/src/typescript.ts
@@ -1,6 +1,8 @@
 // Don't tell anyone I stole this from
 // https://github.com/Microsoft/TypeScriptSamples/blob/master/greeter/greeter.ts
 
+let add = function(x: number, y: number): number { return  x + y + 1; };
+
 class Greeter {
   constructor(public greeting: string) { }
   greet() {
@@ -8,6 +10,6 @@ class Greeter {
   }
 };
 
-var greeter = new Greeter("Yo soy Typescript!");
-var el = document.getElementById('typescript');
+const greeter = new Greeter(`Yo soy Typescript! <em>2 + 2 = ${add(2,2)}</em>`);
+let el = document.getElementById('typescript');
 el.innerHTML = greeter.greet();
