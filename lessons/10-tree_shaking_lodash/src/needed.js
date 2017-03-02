17292ddb8173b9e56533f0f8ba0c312bcaabdc94
Peter Cho <pcho231@gmail.com>
2017-03-02 00:55:34 -0500

    Added lodash example

---
+++ lessons/10-tree_shaking_lodash/src/needed.js
@@ -0,0 +1,5 @@
+
+export default function needed(first, second) { console.log('needed'); return first + second };
+
+export function unneeded1(third, fourth) { console.log('unneeded1'); return third * fourth };
+export function unneeded2(fifth, sixth) { console.log('unneeded2'); return fifth / sixth };
---
+++ lessons/10-tree_shaking_lodash/src/scripts.js
@@ -0,0 +1,4 @@
+
+import _ from 'lodash';
+
+console.log(_.compact([0,0,1,0,0]));
