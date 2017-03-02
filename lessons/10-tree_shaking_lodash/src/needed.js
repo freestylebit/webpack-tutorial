d0e1f9c482900a128570b3cd5164e052c098db6b
Peter Cho <pcho231@gmail.com>
2017-03-02 00:56:05 -0500

    Removed unnecessary file

--- lessons/10-tree_shaking_lodash/src/needed.js
+++
@@ -1,5 +0,0 @@
-
-export default function needed(first, second) { console.log('needed'); return first + second };
-
-export function unneeded1(third, fourth) { console.log('unneeded1'); return third * fourth };
-export function unneeded2(fifth, sixth) { console.log('unneeded2'); return fifth / sixth };
--- lessons/10-tree_shaking_lodash/src/scripts.js
+++ lessons/10-tree_shaking_lodash/src/scripts.js
@@ -1,4 +1,4 @@
 
-import _ from 'lodash';
+import _ from 'lodash-es';
 
 console.log(_.compact([0,0,1,0,0]));
